bb9828ad0cb19dc35c771cef1437b4d5a2d97e30
kevinville <kevinlhardyal@gmail.com>
2023-02-09 21:33:36 -0500

    Updated Activity 4 files for requirements

--- Program.cs
+++ Program.cs
@@ -1,40 +1,65 @@
 ﻿using System.Reflection.Metadata.Ecma335;
 using System.Runtime.CompilerServices;
+using System.Collections;
 
-interface KrustyKrab
-{
-    void cookPatty();
-    void visitChumBucket();
-    void TakeOrders();
-}
 
-class Spongebob: KrustyKrab
-{
-    public void cookPatty()
+    interface KrustyKrab
     {
-        Console.WriteLine("Patty is now on Grill!");
+        string? FirstName { get; set; }
+        string? LastName { get; set; }
+        string? WorkerData { get; }
+        int Salary { get; set; }
+        void returninfo();
     }
-    public void visitChumBucket()
-    {
-        Console.WriteLine("Arrived at front entrance of the chum bucket.");
-    }
-    public void TakeOrders()
-    {
-        Console.WriteLine("1 Krabby deluxe and kelp shake.");
-    }
-}
 
-public partial class Program
-{
-    static void Main(string[] args)
+    class Spongebob : KrustyKrab
     {
-        Spongebob squidward= new Spongebob();
-        Spongebob squarepants = new Spongebob();
-        Spongebob plankton = new Spongebob();
 
-        squidward.TakeOrders();
-        squarepants.cookPatty();
-        plankton.visitChumBucket();
+        ArrayList workerInfo = new ArrayList();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Salary { get; set; }
+        public string WorkerData
+        {
+           get{ return "First Name: " + FirstName + " Last Name: " + LastName + " Total Pay: " + Salary; }
+        }
+
+        public void returninfo()
+        {
+            Console.WriteLine(WorkerData);
+        }
+
+        public KrustyKrab this[int index]
+        {
+            get => (KrustyKrab)workerInfo[index];
+            set => workerInfo.Insert(index, value);
+        }
 
     }
-}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Spongebob workerInfo = new Spongebob();
+            workerInfo[0] = new Spongebob();
+            workerInfo[0].FirstName = "Squidward";
+            workerInfo[0].LastName = "Tentacles";
+            workerInfo[0].Salary = 31513;
+
+            workerInfo[1] = new Spongebob();
+            workerInfo[1].FirstName = "SPongebob";
+            workerInfo[1].LastName = "Squarepants";
+            workerInfo[0].Salary = 46246;
+
+            workerInfo[2] = new Spongebob();
+            workerInfo[2].FirstName = "Eugene";
+            workerInfo[2].LastName = "Krabs";
+            workerInfo[0].Salary = 42624;
+
+            for (int i = 0; i < 3; i++)
+            {
+                workerInfo[i].returninfo();
+            }
+
+        }
+    }
