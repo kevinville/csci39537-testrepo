7458c7792c997da9d8c53f2cd0787c1b321a0cee
kevinville <kevinlhardyal@gmail.com>
2023-02-09 19:32:46 -0500

    Added activity 4 code

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+
+interface KrustyKrab
+{
+    void cookPatty();
+    void visitChumBucket();
+    void TakeOrders();
+}
+
+class Spongebob: KrustyKrab
+{
+    public void cookPatty()
+    {
+        Console.WriteLine("Patty is now on Grill!");
+    }
+    public void visitChumBucket()
+    {
+        Console.WriteLine("Arrived at front entrance of the chum bucket.");
+    }
+    public void TakeOrders()
+    {
+        Console.WriteLine("1 Krabby deluxe and kelp shake.");
+    }
+}
+
+public partial class Program
+{
+    static void Main(string[] args)
+    {
+        Spongebob squidward= new Spongebob();
+        Spongebob squarepants = new Spongebob();
+        Spongebob plankton = new Spongebob();
+
+        squidward.TakeOrders();
+        squarepants.cookPatty();
+        plankton.visitChumBucket();
+
+    }
+}
